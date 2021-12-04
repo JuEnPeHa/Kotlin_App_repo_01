46aee69476bcf7d0a8ecc8f63b9ebfe0066d3fc9
JuEnPeHa <j.enrique.23121996@gmail.com>
2021-12-04 00:47:30 -0600

    Initial commit

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.happybirthday
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		val boton: Button = findViewById<Button>(R.id.button_to_second)
+		boton.setOnClickListener{val intent = Intent(this, MainActivity2::class.java)
+		startActivity(intent)}
+	}
+}
